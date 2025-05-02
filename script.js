034f4f099a52ad49096daffdfd042ee069a50a91
Read Langworthy <rlangworthy@gmail.com>
2025-05-02 11:26:16 -0700

    completing the form

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 console.log("Hello, world!");
 
 
-document.getElementById("start-button").addEventListener("click", function() {
-    console.log("Button clicked!");
-    document.getElementById("fire-planner").style.display = "block";
+
+// Shows and outputs the tax range slider value in the output field
+const taxRateInputSlider = document.getElementById('tax-rate-input');
+const taxRateSliderOutput = document.getElementById('tax-rate-output');
+taxRateInputSlider.addEventListener('input', function() {
+    taxRateSliderOutput.value = `${taxRateInputSlider.value}%`;
+});
+
+// Shows and outputs the hysa range slider value in the output field
+const hysaRateInputSlider = document.getElementById('hysa-rate-input');
+const hysaRateSliderOutput = document.getElementById('hysa-rate-output');
+hysaRateInputSlider.addEventListener('input', function() {
+    hysaRateSliderOutput.value = `${hysaRateInputSlider.value}%`;
+});
+
+// Shows and outputs the Investments rate range slider value in the output field
+const investmentsRateInputSlider = document.getElementById('investments-rate-input');
+const investmentsRateSliderOutput = document.getElementById('investments-rate-output');
+investmentsRateInputSlider.addEventListener('input', function() {
+    investmentsRateSliderOutput.value = `${investmentsRateInputSlider.value}%`;
 });
