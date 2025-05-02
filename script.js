9a8c5acb2de4484bc7066cbf4525d946a6324491
Read Langworthy <rlangworthy@gmail.com>
2025-05-02 08:56:15 -0700

    updates

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Hello, world!");
+
+
+document.getElementById("start-button").addEventListener("click", function() {
+    console.log("Button clicked!");
+    document.getElementById("fire-planner").style.display = "block";
+});
