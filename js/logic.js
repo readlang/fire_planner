9b7827ac3f1433ccddb273a22735d00705fc3b08
Read Langworthy <rlangworthy@gmail.com>
2025-05-23 15:29:16 -0700

    updates

--- js/logic.js
+++ js/logic.js
@@ -1,39 +1,55 @@
 
 let financialData = {
     ages: [],
+    cash: [],
     savings: [],
-    hysa: [],
     investments: [],
     totals: [],
 };
 
-function round(number) {
+function roundTwo(number) {
     return Math.round((number + Number.EPSILON) * 100) / 100
 }
 
+function roundOne(number) {
+    return Math.round((number + Number.EPSILON) * 10) / 10
+}
+
+
+function setMix() {
+    let total = userData.cash + userData.savings + userData.investments;
+    if (total > 0) {
+        userData.assetMix = {
+            cash: userData.cash / total,
+            savings: userData.savings / total,
+            investments: userData.investments / total,
+        }
+    } else userData.assetMix = {cash: 0, savings: 0, investments: 0}
+}
+
 function setValues() {
-    
     financialData = {
         ages: [userData.age],
+        cash: [userData.cash],
         savings: [userData.savings],
-        hysa: [userData.hysa],
         investments: [userData.investments],
-        totals: [userData.savings + userData.hysa + userData.investments]
+        totals: [userData.cash + userData.savings + userData.investments]
     };
 
     for (let age = userData.age + 1, i=1; age <= userData.ageGraphEnd; age++, i++) {
 
         const netIncome = userData.income * (1 - (userData.taxRate * 0.01)) - (userData.annualExpenses + userData.monthlyExpenses * 12);
+        // This is a placeholder value
         const percentToInvest = 0.60;
         
-        const newSavings = round(financialData.savings[i-1]);
-        const newHysa = round(financialData.hysa[i-1] * (1 + userData.hysaRate * .01) + ((1 - percentToInvest) * netIncome));
-        const newInvestments = round(financialData.investments[i-1] * (1 + userData.investmentsRate * .01) + (percentToInvest * netIncome));
-        const newTotal = round(newSavings + newHysa + newInvestments);
+        const newCash = roundTwo(financialData.cash[i-1]);
+        const newSavings = roundTwo(financialData.savings[i-1] * (1 + userData.savingsRate * .01) + ((1 - percentToInvest) * netIncome));
+        const newInvestments = roundTwo(financialData.investments[i-1] * (1 + userData.investmentsRate * .01) + (percentToInvest * netIncome));
+        const newTotal = roundTwo(newCash + newSavings + newInvestments);
 
         financialData.ages.push(age)
+        financialData.cash.push(newCash)
         financialData.savings.push(newSavings)
-        financialData.hysa.push(newHysa)
         financialData.investments.push(newInvestments)
         financialData.totals.push(newTotal)
     }
@@ -42,6 +58,7 @@ function setValues() {
 
 // set the initial values for financialData
 setValues();
+setMix();
 
 // to refresh the values and chart on Calculate button press
 function refreshData() {
--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let userData = {}
 
+// Duration Age Data
 const ageInput = document.getElementById('age-input');
 userData.age = parseInt(ageInput.value);
 ageInput.addEventListener('input', () => userData.age = parseInt(ageInput.value));
@@ -12,6 +13,7 @@ const graphEndAgeInput = document.getElementById('graph-end-age-input');
 userData.ageGraphEnd = parseInt(graphEndAgeInput.value);
 graphEndAgeInput.addEventListener('input', () => userData.ageGraphEnd = parseInt(graphEndAgeInput.value));
 
+// Income & Expenses
 const incomeInput = document.getElementById('income-input');
 userData.income = parseInt(incomeInput.value);
 incomeInput.addEventListener('input', () => userData.income = parseInt(incomeInput.value));
@@ -28,17 +30,18 @@ const annualExpensesInput = document.getElementById('annual-expenses-input');
 userData.annualExpenses = parseInt(annualExpensesInput.value);
 annualExpensesInput.addEventListener('input', () => userData.annualExpenses = parseInt(annualExpensesInput.value));
 
+// Assets
+const cashInput = document.getElementById('cash-input');
+userData.cash = parseInt(cashInput.value);
+cashInput.addEventListener('input', () => userData.cash = parseInt(cashInput.value));
+
 const savingsInput = document.getElementById('savings-input');
 userData.savings = parseInt(savingsInput.value);
 savingsInput.addEventListener('input', () => userData.savings = parseInt(savingsInput.value));
 
-const hysaInput = document.getElementById('hysa-input');
-userData.hysa = parseInt(hysaInput.value);
-hysaInput.addEventListener('input', () => userData.hysa = parseInt(hysaInput.value));
-
-const hysaRateInput = document.getElementById('hysa-rate-input');
-userData.hysaRate = parseFloat(hysaRateInput.value);
-hysaRateInput.addEventListener('input', () => userData.hysaRate = parseFloat(hysaRateInput.value));
+const savingsRateInput = document.getElementById('savings-rate-input');
+userData.savingsRate = parseFloat(savingsRateInput.value);
+savingsRateInput.addEventListener('input', () => userData.savingsRate = parseFloat(savingsRateInput.value));
 
 const investmentsInput = document.getElementById('investments-input');
 userData.investments = parseInt(investmentsInput.value);
@@ -48,13 +51,33 @@ const investmentsRateInput = document.getElementById('investments-rate-input');
 userData.investmentsRate = parseFloat(investmentsRateInput.value);
 investmentsRateInput.addEventListener('input', () => userData.investmentsRate = parseFloat(investmentsRateInput.value));
 
+// controls the asset mix display
+const cashPercent = document.getElementById("cash-percent");
+const savingsPercent = document.getElementById("savings-percent");
+const investmentPercent = document.getElementById("investment-percent");
+
+document.getElementById("input-form").addEventListener("click", () => {
+    setMix()
+    cashPercent.value = `${roundOne(userData.assetMix.cash * 100)}%`
+    savingsPercent.value = `${roundOne(userData.assetMix.savings * 100)}%`
+    investmentPercent.value = `${roundOne(userData.assetMix.investments * 100)}%`
+})
+
+// Retirement Spending
+const withdrawalRateInput = document.getElementById("withdrawal-rate-input");
+userData.withdrawalRate = parseFloat(withdrawalRateInput.value);
+withdrawalRateInput.addEventListener("input", ()=> userData.withdrawalRate = parseFloat(withdrawalRateInput.value));
+
+const withdrawalRateRateOutput = document.getElementById('withdrawal-rate-output');
+withdrawalRateInput.addEventListener("input", () => withdrawalRateRateOutput.value = `${withdrawalRateInput.value}%`);
+
 // Shows and outputs the tax range slider value in the output field
 const taxRateSliderOutput = document.getElementById('tax-rate-output');
 taxRateInput.addEventListener('input', () => taxRateSliderOutput.value = `${taxRateInput.value}%`);
 
-// Shows and outputs the hysa range slider value in the output field
-const hysaRateSliderOutput = document.getElementById('hysa-rate-output');
-hysaRateInput.addEventListener('input', () => hysaRateSliderOutput.value = `${hysaRateInput.value}%` );
+// Shows and outputs the savings range slider value in the output field
+const savingsRateSliderOutput = document.getElementById('savings-rate-output');
+savingsRateInput.addEventListener('input', () => savingsRateSliderOutput.value = `${savingsRateInput.value}%` );
 
 // Shows and outputs the Investments rate range slider value in the output field
 const investmentsRateSliderOutput = document.getElementById('investments-rate-output');
@@ -70,3 +93,4 @@ calculateButton.addEventListener('click', (e) => {
 
 
 
+console.log(userData)
