a71a61e7a56ec1e6b4c8dc32c23783eb5287caf8
Read Langworthy <rlangworthy@gmail.com>
2025-05-29 13:04:20 -0700

    converted to TS

--- js/index.js
+++ js/index.js
@@ -1,27 +1,37 @@
 "use strict";
-// TS Experimental File
-// -script-
 let userData = {
-    "age": 41,
-    "ageRetirement": 60,
-    "ageGraphEnd": 50,
-    "income": 75000,
-    "taxRate": 28,
-    "monthlyExpenses": 4000,
-    "annualExpenses": 8000,
-    "cash": 10000,
-    "savings": 20000,
-    "savingsRate": 3.5,
-    "investments": 50000,
-    "investmentsRate": 7,
-    "withdrawalRate": 4,
-    "assetMix": {
-        "cash": 0.125,
-        "savings": 0.25,
-        "investments": 0.625
+    age: 40,
+    ageRetirement: 60,
+    ageGraphEnd: 80,
+    income: 75000,
+    taxRate: 28,
+    monthlyExpenses: 4000,
+    annualExpenses: 8000,
+    cash: 10000,
+    savings: 20000,
+    savingsRate: 3.5,
+    investments: 50000,
+    investmentsRate: 7,
+    withdrawalRate: 4,
+    assetMix: {
+        cash: 0.125,
+        savings: 0.25,
+        investments: 0.625
     }
 };
-// Duration Age Data
+/**
+ * Function removes non-numeric chars from string input (numbers and decimal pts remain)
+ * @param inputString the input string
+ * @returns a number as a string type (if blank, a "0" will be returned)
+ */
+function removeNonNumeric(inputString) {
+    let modifiedStr = inputString.replace(/[^0-9.]/g, '');
+    if (modifiedStr == "")
+        modifiedStr = "0";
+    //console.log(value);
+    return modifiedStr;
+}
+// DURATION Age Data inputs
 const ageInput = document.getElementById('age-input');
 userData.age = parseInt(ageInput.value);
 ageInput.addEventListener('input', () => userData.age = parseInt(ageInput.value));
@@ -31,61 +41,83 @@ retirementAgeInput.addEventListener('input', () => userData.ageRetirement = pars
 const graphEndAgeInput = document.getElementById('graph-end-age-input');
 userData.ageGraphEnd = parseInt(graphEndAgeInput.value);
 graphEndAgeInput.addEventListener('input', () => userData.ageGraphEnd = parseInt(graphEndAgeInput.value));
-// Income & Expenses
+// INCOME & EXPENSES
 const incomeInput = document.getElementById('income-input');
-userData.income = parseInt(incomeInput.value);
-incomeInput.addEventListener('input', () => userData.income = parseInt(incomeInput.value));
+userData.income = parseInt(removeNonNumeric(incomeInput.value));
+incomeInput.addEventListener('input', () => {
+    userData.income = parseInt(removeNonNumeric(incomeInput.value));
+    incomeInput.value = (userData.income).toLocaleString();
+});
 const taxRateInput = document.getElementById('tax-rate-input');
+const taxRateSliderOutput = document.getElementById('tax-rate-output');
 userData.taxRate = parseFloat(taxRateInput.value);
-taxRateInput.addEventListener('input', () => userData.taxRate = parseFloat(taxRateInput.value));
+taxRateInput.addEventListener('input', () => {
+    userData.taxRate = parseFloat(taxRateInput.value);
+    taxRateSliderOutput.value = `${taxRateInput.value}%`;
+});
 const monthlyExpensesInput = document.getElementById('monthly-expenses-input');
-userData.monthlyExpenses = parseInt(monthlyExpensesInput.value);
-monthlyExpensesInput.addEventListener('input', () => userData.monthlyExpenses = parseInt(monthlyExpensesInput.value));
+userData.monthlyExpenses = parseInt(removeNonNumeric(monthlyExpensesInput.value));
+monthlyExpensesInput.addEventListener('input', () => {
+    userData.monthlyExpenses = parseInt(removeNonNumeric(monthlyExpensesInput.value));
+    monthlyExpensesInput.value = userData.monthlyExpenses.toLocaleString();
+});
 const annualExpensesInput = document.getElementById('annual-expenses-input');
-userData.annualExpenses = parseInt(annualExpensesInput.value);
-annualExpensesInput.addEventListener('input', () => userData.annualExpenses = parseInt(annualExpensesInput.value));
-// Assets
+userData.annualExpenses = parseInt(removeNonNumeric(annualExpensesInput.value));
+annualExpensesInput.addEventListener('input', () => {
+    userData.annualExpenses = parseInt(removeNonNumeric(annualExpensesInput.value));
+    annualExpensesInput.value = userData.annualExpenses.toLocaleString();
+});
+// ASSETS
 const cashInput = document.getElementById('cash-input');
-userData.cash = parseInt(cashInput.value);
-cashInput.addEventListener('input', () => userData.cash = parseInt(cashInput.value));
+userData.cash = parseInt(removeNonNumeric(cashInput.value));
+cashInput.addEventListener('input', () => {
+    userData.cash = parseInt(removeNonNumeric(cashInput.value));
+    cashInput.value = userData.cash.toLocaleString();
+});
 const savingsInput = document.getElementById('savings-input');
-userData.savings = parseInt(savingsInput.value);
-savingsInput.addEventListener('input', () => userData.savings = parseInt(savingsInput.value));
+userData.savings = parseInt(removeNonNumeric(savingsInput.value));
+savingsInput.addEventListener('input', () => {
+    userData.savings = parseInt(removeNonNumeric(savingsInput.value));
+    savingsInput.value = userData.savings.toLocaleString();
+});
 const savingsRateInput = document.getElementById('savings-rate-input');
+const savingsRateSliderOutput = document.getElementById('savings-rate-output');
 userData.savingsRate = parseFloat(savingsRateInput.value);
-savingsRateInput.addEventListener('input', () => userData.savingsRate = parseFloat(savingsRateInput.value));
+savingsRateInput.addEventListener('input', () => {
+    userData.savingsRate = parseFloat(savingsRateInput.value);
+    savingsRateSliderOutput.value = `${savingsRateInput.value}%`;
+});
 const investmentsInput = document.getElementById('investments-input');
-userData.investments = parseInt(investmentsInput.value);
-investmentsInput.addEventListener('input', () => userData.investments = parseInt(investmentsInput.value));
+userData.investments = parseInt(removeNonNumeric(investmentsInput.value));
+investmentsInput.addEventListener('input', () => {
+    userData.investments = parseInt(removeNonNumeric(investmentsInput.value));
+    investmentsInput.value = userData.investments.toLocaleString();
+});
 const investmentsRateInput = document.getElementById('investments-rate-input');
+const investmentsRateSliderOutput = document.getElementById('investments-rate-output');
 userData.investmentsRate = parseFloat(investmentsRateInput.value);
-investmentsRateInput.addEventListener('input', () => userData.investmentsRate = parseFloat(investmentsRateInput.value));
+investmentsRateInput.addEventListener('input', () => {
+    userData.investmentsRate = parseFloat(investmentsRateInput.value);
+    investmentsRateSliderOutput.value = `${investmentsRateInput.value}%`;
+});
 // controls the asset mix display
 const cashPercent = document.getElementById("cash-percent");
 const savingsPercent = document.getElementById("savings-percent");
 const investmentPercent = document.getElementById("investment-percent");
 const entireForm = document.getElementById("input-form");
-entireForm.addEventListener("click", () => {
-    setMix();
-    cashPercent.value = `${roundOne(userData.assetMix.cash * 100)}%`;
-    savingsPercent.value = `${roundOne(userData.assetMix.savings * 100)}%`;
-    investmentPercent.value = `${roundOne(userData.assetMix.investments * 100)}%`;
+entireForm.addEventListener("input", () => {
+    setAssetMix();
+    cashPercent.value = `${round(1, userData.assetMix.cash * 100)}%`;
+    savingsPercent.value = `${round(1, userData.assetMix.savings * 100)}%`;
+    investmentPercent.value = `${round(1, userData.assetMix.investments * 100)}%`;
 });
-// Retirement Spending
+// RETIREMENT SPENDING
 const withdrawalRateInput = document.getElementById("withdrawal-rate-input");
 userData.withdrawalRate = parseFloat(withdrawalRateInput.value);
 withdrawalRateInput.addEventListener("input", () => userData.withdrawalRate = parseFloat(withdrawalRateInput.value));
 const withdrawalRateRateOutput = document.getElementById('withdrawal-rate-output');
 withdrawalRateInput.addEventListener("input", () => withdrawalRateRateOutput.value = `${withdrawalRateInput.value}%`);
-// Shows and outputs the tax range slider value in the output field
-const taxRateSliderOutput = document.getElementById('tax-rate-output');
-taxRateInput.addEventListener('input', () => taxRateSliderOutput.value = `${taxRateInput.value}%`);
-// Shows and outputs the savings range slider value in the output field
-const savingsRateSliderOutput = document.getElementById('savings-rate-output');
-savingsRateInput.addEventListener('input', () => savingsRateSliderOutput.value = `${savingsRateInput.value}%`);
-// Shows and outputs the Investments rate range slider value in the output field
-const investmentsRateSliderOutput = document.getElementById('investments-rate-output');
-investmentsRateInput.addEventListener('input', () => investmentsRateSliderOutput.value = `${investmentsRateInput.value}%`);
+// FORM SUBMIT buttons
 // controls behavior of clicking Calculate button
 const calculateButton = document.getElementById('calculate');
 calculateButton.addEventListener('click', (e) => {
@@ -93,21 +125,20 @@ calculateButton.addEventListener('click', (e) => {
     console.log(userData);
     refreshData();
 });
-console.log(userData);
-let financialData = {
-    ages: [],
-    cash: [],
-    savings: [],
-    investments: [],
-    totals: [],
-};
-function roundTwo(number) {
-    return Math.round((number + Number.EPSILON) * 100) / 100;
+// ---- Logic of app starts here (above relates to html form inputs) ----
+/**
+ * Rounds a number to N decimal places
+ * @param N - number of decimal places
+ * @param inputNumber - the number to round
+ * @returns a number rounded to N decimal places
+ */
+function round(N, inputNumber) {
+    return Math.round((inputNumber + Number.EPSILON) * (Math.pow(10, N))) / (Math.pow(10, N));
 }
-function roundOne(number) {
-    return Math.round((number + Number.EPSILON) * 10) / 10;
-}
-function setMix() {
+/**
+ * Sets the asset mix (cash / savings / investments) in userData.assetMix
+ */
+function setAssetMix() {
     let total = userData.cash + userData.savings + userData.investments;
     if (total > 0) {
         userData.assetMix = {
@@ -119,7 +150,20 @@ function setMix() {
     else
         userData.assetMix = { cash: 0, savings: 0, investments: 0 };
 }
+let financialData = {
+    ages: [],
+    cash: [],
+    savings: [],
+    investments: [],
+    totals: [],
+};
+/**
+ * Calculates and sets the values for future year asset totals
+ * that will be used to create the Chart graphic
+ */
 function setValues() {
+    setAssetMix();
+    // this sets the initial year values in each array
     financialData = {
         ages: [userData.age],
         cash: [userData.cash],
@@ -128,13 +172,13 @@ function setValues() {
         totals: [userData.cash + userData.savings + userData.investments]
     };
     for (let age = userData.age + 1, i = 1; age <= userData.ageGraphEnd; age++, i++) {
-        const netIncome = userData.income * (1 - (userData.taxRate * 0.01)) - (userData.annualExpenses + userData.monthlyExpenses * 12);
+        const netIncome = (userData.income * (1 - userData.taxRate * 0.01)) - (userData.annualExpenses + (userData.monthlyExpenses * 12));
         // This is a placeholder value
         const percentToInvest = 0.60;
-        const newCash = roundTwo(financialData.cash[i - 1]);
-        const newSavings = roundTwo(financialData.savings[i - 1] * (1 + userData.savingsRate * .01) + ((1 - percentToInvest) * netIncome));
-        const newInvestments = roundTwo(financialData.investments[i - 1] * (1 + userData.investmentsRate * .01) + (percentToInvest * netIncome));
-        const newTotal = roundTwo(newCash + newSavings + newInvestments);
+        const newCash = round(2, financialData.cash[i - 1]);
+        const newSavings = round(2, financialData.savings[i - 1] * (1 + userData.savingsRate * .01) + ((1 - percentToInvest) * netIncome));
+        const newInvestments = round(2, financialData.investments[i - 1] * (1 + userData.investmentsRate * .01) + (percentToInvest * netIncome));
+        const newTotal = round(2, newCash + newSavings + newInvestments);
         financialData.ages.push(age);
         financialData.cash.push(newCash);
         financialData.savings.push(newSavings);
@@ -143,10 +187,11 @@ function setValues() {
     }
     console.log(financialData);
 }
-// set the initial values for financialData
+// set the initial values for financialData 
 setValues();
-setMix();
-// to refresh the values and chart on Calculate button press
+/**
+ * to refresh the values and chart on Calculate button press
+ */
 function refreshData() {
     setValues();
     chartGraphic.data.labels = financialData.ages;
--- js/index.ts
+++ js/index.ts
@@ -1,31 +1,59 @@
-// TS Experimental File
 
-// -script-
+interface UserData {
+    age: number;
+    ageRetirement: number;
+    ageGraphEnd: number;
+    income: number;
+    taxRate: number;
+    monthlyExpenses: number;
+    annualExpenses: number;
+    cash: number;
+    savings: number;
+    savingsRate: number;
+    investments: number;
+    investmentsRate: number;
+    withdrawalRate: number;
+    assetMix: {
+        cash: number;
+        savings: number;
+        investments: number;
+    }
+}
 
-let userData = {
-    "age": 41,
-    "ageRetirement": 60,
-    "ageGraphEnd": 50,
-    "income": 75000,
-    "taxRate": 28,
-    "monthlyExpenses": 4000,
-    "annualExpenses": 8000,
-    "cash": 10000,
-    "savings": 20000,
-    "savingsRate": 3.5,
-    "investments": 50000,
-    "investmentsRate": 7,
-    "withdrawalRate": 4,
-    "assetMix": {
-        "cash": 0.125,
-        "savings": 0.25,
-        "investments": 0.625
+let userData: UserData = {
+    age: 40,
+    ageRetirement: 60,
+    ageGraphEnd: 80,
+    income: 75000,
+    taxRate: 28,
+    monthlyExpenses: 4000,
+    annualExpenses: 8000,
+    cash: 10000,
+    savings: 20000,
+    savingsRate: 3.5,
+    investments: 50000,
+    investmentsRate: 7,
+    withdrawalRate: 4,
+    assetMix: {
+        cash: 0.125,
+        savings: 0.25,
+        investments: 0.625
     }
 };
 
+/**
+ * Function removes non-numeric chars from string input (numbers and decimal pts remain)
+ * @param inputString the input string
+ * @returns a number as a string type (if blank, a "0" will be returned)
+ */
+function removeNonNumeric(inputString: string): string {
+  let modifiedStr =  inputString.replace(/[^0-9.]/g, '');
+  if (modifiedStr == "") modifiedStr = "0";
+  //console.log(value);
+  return modifiedStr
+}
 
-
-// Duration Age Data
+// DURATION Age Data inputs
 const ageInput = document.getElementById('age-input') as HTMLInputElement;
 userData.age = parseInt(ageInput.value);
 ageInput.addEventListener('input', () => userData.age = parseInt(ageInput.value));
@@ -38,47 +66,73 @@ const graphEndAgeInput = document.getElementById('graph-end-age-input') as HTMLI
 userData.ageGraphEnd = parseInt(graphEndAgeInput.value);
 graphEndAgeInput.addEventListener('input', () => userData.ageGraphEnd = parseInt(graphEndAgeInput.value));
 
-// Income & Expenses
+// INCOME & EXPENSES
 const incomeInput = document.getElementById('income-input') as HTMLInputElement;
-userData.income = parseInt(incomeInput.value);
-incomeInput.addEventListener('input', () => userData.income = parseInt(incomeInput.value));
-
-
-
-
+userData.income = parseInt(removeNonNumeric(incomeInput.value));
+incomeInput.addEventListener('input', () => {
+    userData.income = parseInt(removeNonNumeric(incomeInput.value));
+    incomeInput.value = (userData.income).toLocaleString();
+});
 
 const taxRateInput = document.getElementById('tax-rate-input') as HTMLInputElement;
+const taxRateSliderOutput = document.getElementById('tax-rate-output') as HTMLOutputElement;
 userData.taxRate = parseFloat(taxRateInput.value);
-taxRateInput.addEventListener('input', () => userData.taxRate = parseFloat(taxRateInput.value));
+taxRateInput.addEventListener('input', () => {
+    userData.taxRate = parseFloat(taxRateInput.value)
+    taxRateSliderOutput.value = `${taxRateInput.value}%`
+});
 
 const monthlyExpensesInput = document.getElementById('monthly-expenses-input') as HTMLInputElement;
-userData.monthlyExpenses = parseInt(monthlyExpensesInput.value);
-monthlyExpensesInput.addEventListener('input', () => userData.monthlyExpenses = parseInt(monthlyExpensesInput.value));
+userData.monthlyExpenses = parseInt(removeNonNumeric(monthlyExpensesInput.value));
+monthlyExpensesInput.addEventListener('input', () => {
+    userData.monthlyExpenses = parseInt(removeNonNumeric(monthlyExpensesInput.value));
+    monthlyExpensesInput.value = userData.monthlyExpenses.toLocaleString();
+});
 
 const annualExpensesInput = document.getElementById('annual-expenses-input') as HTMLInputElement;
-userData.annualExpenses = parseInt(annualExpensesInput.value);
-annualExpensesInput.addEventListener('input', () => userData.annualExpenses = parseInt(annualExpensesInput.value));
+userData.annualExpenses = parseInt(removeNonNumeric(annualExpensesInput.value));
+annualExpensesInput.addEventListener('input', () => {
+    userData.annualExpenses = parseInt(removeNonNumeric(annualExpensesInput.value))
+    annualExpensesInput.value = userData.annualExpenses.toLocaleString();
+});
 
-// Assets
+// ASSETS
 const cashInput = document.getElementById('cash-input') as HTMLInputElement;
-userData.cash = parseInt(cashInput.value);
-cashInput.addEventListener('input', () => userData.cash = parseInt(cashInput.value));
+userData.cash = parseInt(removeNonNumeric(cashInput.value));
+cashInput.addEventListener('input', () => {
+    userData.cash = parseInt(removeNonNumeric(cashInput.value));
+    cashInput.value = userData.cash.toLocaleString();
+});
 
 const savingsInput = document.getElementById('savings-input') as HTMLInputElement;
-userData.savings = parseInt(savingsInput.value);
-savingsInput.addEventListener('input', () => userData.savings = parseInt(savingsInput.value));
+userData.savings = parseInt(removeNonNumeric(savingsInput.value));
+savingsInput.addEventListener('input', () => {
+    userData.savings = parseInt(removeNonNumeric(savingsInput.value));
+    savingsInput.value = userData.savings.toLocaleString();
+});
 
 const savingsRateInput = document.getElementById('savings-rate-input') as HTMLInputElement;
+const savingsRateSliderOutput = document.getElementById('savings-rate-output') as HTMLOutputElement;
 userData.savingsRate = parseFloat(savingsRateInput.value);
-savingsRateInput.addEventListener('input', () => userData.savingsRate = parseFloat(savingsRateInput.value));
+savingsRateInput.addEventListener('input', () => {
+    userData.savingsRate = parseFloat(savingsRateInput.value)
+    savingsRateSliderOutput.value = `${savingsRateInput.value}%` 
+});
 
 const investmentsInput = document.getElementById('investments-input') as HTMLInputElement;
-userData.investments = parseInt(investmentsInput.value);
-investmentsInput.addEventListener('input', () => userData.investments = parseInt(investmentsInput.value));
+userData.investments = parseInt(removeNonNumeric(investmentsInput.value));
+investmentsInput.addEventListener('input', () => {
+    userData.investments = parseInt(removeNonNumeric(investmentsInput.value));
+    investmentsInput.value = userData.investments.toLocaleString();
+});
 
 const investmentsRateInput = document.getElementById('investments-rate-input') as HTMLInputElement;
+const investmentsRateSliderOutput = document.getElementById('investments-rate-output') as HTMLOutputElement;
 userData.investmentsRate = parseFloat(investmentsRateInput.value);
-investmentsRateInput.addEventListener('input', () => userData.investmentsRate = parseFloat(investmentsRateInput.value));
+investmentsRateInput.addEventListener('input', () => {
+    userData.investmentsRate = parseFloat(investmentsRateInput.value)
+    investmentsRateSliderOutput.value = `${investmentsRateInput.value}%`
+});
 
 // controls the asset mix display
 const cashPercent = document.getElementById("cash-percent") as HTMLOutputElement;
@@ -86,33 +140,21 @@ const savingsPercent = document.getElementById("savings-percent") as HTMLOutputE
 const investmentPercent = document.getElementById("investment-percent") as HTMLOutputElement;
 
 const entireForm = document.getElementById("input-form") as HTMLInputElement;
-entireForm.addEventListener("click", () => {
-    setMix()
-    cashPercent.value = `${roundOne(userData.assetMix.cash * 100)}%`
-    savingsPercent.value = `${roundOne(userData.assetMix.savings * 100)}%`
-    investmentPercent.value = `${roundOne(userData.assetMix.investments * 100)}%`
+entireForm.addEventListener("input", () => {
+    setAssetMix()
+    cashPercent.value = `${round(1, userData.assetMix.cash * 100)}%`
+    savingsPercent.value = `${round(1, userData.assetMix.savings * 100)}%`
+    investmentPercent.value = `${round(1, userData.assetMix.investments * 100)}%`
 })
 
-// Retirement Spending
+// RETIREMENT SPENDING
 const withdrawalRateInput = document.getElementById("withdrawal-rate-input") as HTMLInputElement;
 userData.withdrawalRate = parseFloat(withdrawalRateInput.value);
 withdrawalRateInput.addEventListener("input", ()=> userData.withdrawalRate = parseFloat(withdrawalRateInput.value));
-
 const withdrawalRateRateOutput = document.getElementById('withdrawal-rate-output') as HTMLOutputElement;
 withdrawalRateInput.addEventListener("input", () => withdrawalRateRateOutput.value = `${withdrawalRateInput.value}%`);
 
-// Shows and outputs the tax range slider value in the output field
-const taxRateSliderOutput = document.getElementById('tax-rate-output') as HTMLOutputElement;
-taxRateInput.addEventListener('input', () => taxRateSliderOutput.value = `${taxRateInput.value}%`);
-
-// Shows and outputs the savings range slider value in the output field
-const savingsRateSliderOutput = document.getElementById('savings-rate-output') as HTMLOutputElement;
-savingsRateInput.addEventListener('input', () => savingsRateSliderOutput.value = `${savingsRateInput.value}%` );
-
-// Shows and outputs the Investments rate range slider value in the output field
-const investmentsRateSliderOutput = document.getElementById('investments-rate-output') as HTMLOutputElement;
-investmentsRateInput.addEventListener('input', () => investmentsRateSliderOutput.value = `${investmentsRateInput.value}%`);
-
+// FORM SUBMIT buttons
 // controls behavior of clicking Calculate button
 const calculateButton = document.getElementById('calculate') as HTMLButtonElement;
 calculateButton.addEventListener('click', (e) => {
@@ -121,11 +163,31 @@ calculateButton.addEventListener('click', (e) => {
     refreshData();
 });
 
+// ---- Logic of app starts here (above relates to html form inputs) ----
 
+/**
+ * Rounds a number to N decimal places
+ * @param N - number of decimal places
+ * @param inputNumber - the number to round
+ * @returns a number rounded to N decimal places
+ */
+function round(N: number, inputNumber: number): number {
+    return Math.round((inputNumber + Number.EPSILON) * (10 ** N)) / (10 ** N)
+}
 
-console.log(userData)
-
-// - logic.js -
+/**
+ * Sets the asset mix (cash / savings / investments) in userData.assetMix
+ */
+function setAssetMix(): void {
+    let total = userData.cash + userData.savings + userData.investments;
+    if (total > 0) {
+        userData.assetMix = {
+            cash: userData.cash / total,
+            savings: userData.savings / total,
+            investments: userData.investments / total,
+        }
+    } else userData.assetMix = {cash: 0, savings: 0, investments: 0}
+}
 
 interface FinancialData {
     ages: number[];
@@ -143,27 +205,13 @@ let financialData: FinancialData = {
     totals: [],
 };
 
-function roundTwo(number: number): number {
-    return Math.round((number + Number.EPSILON) * 100) / 100
-}
-
-function roundOne(number: number): number {
-    return Math.round((number + Number.EPSILON) * 10) / 10
-}
-
-
-function setMix() {
-    let total = userData.cash + userData.savings + userData.investments;
-    if (total > 0) {
-        userData.assetMix = {
-            cash: userData.cash / total,
-            savings: userData.savings / total,
-            investments: userData.investments / total,
-        }
-    } else userData.assetMix = {cash: 0, savings: 0, investments: 0}
-}
-
+/**
+ * Calculates and sets the values for future year asset totals
+ * that will be used to create the Chart graphic
+ */
 function setValues() {
+    setAssetMix();
+    // this sets the initial year values in each array
     financialData = {
         ages: [userData.age],
         cash: [userData.cash],
@@ -172,16 +220,19 @@ function setValues() {
         totals: [userData.cash + userData.savings + userData.investments]
     };
 
-    for (let age: number = userData.age + 1, i=1; age <= userData.ageGraphEnd; age++, i++) {
+    for (let age: number = userData.age + 1, i = 1; age <= userData.ageGraphEnd; age++, i++) {
 
-        const netIncome = userData.income * (1 - (userData.taxRate * 0.01)) - (userData.annualExpenses + userData.monthlyExpenses * 12);
+        const netIncome = (userData.income * (1 - userData.taxRate * 0.01)) - (userData.annualExpenses + (userData.monthlyExpenses * 12));
+
+
+        
         // This is a placeholder value
         const percentToInvest = 0.60;
         
-        const newCash = roundTwo(financialData.cash[i-1]);
-        const newSavings = roundTwo(financialData.savings[i-1] * (1 + userData.savingsRate * .01) + ((1 - percentToInvest) * netIncome));
-        const newInvestments = roundTwo(financialData.investments[i-1] * (1 + userData.investmentsRate * .01) + (percentToInvest * netIncome));
-        const newTotal = roundTwo(newCash + newSavings + newInvestments);
+        const newCash = round(2, financialData.cash[i-1]);
+        const newSavings = round(2, financialData.savings[i-1] * (1 + userData.savingsRate * .01) + ((1 - percentToInvest) * netIncome));
+        const newInvestments = round(2, financialData.investments[i-1] * (1 + userData.investmentsRate * .01) + (percentToInvest * netIncome));
+        const newTotal = round(2, newCash + newSavings + newInvestments);
 
         financialData.ages.push(age)
         financialData.cash.push(newCash)
@@ -192,11 +243,12 @@ function setValues() {
     console.log(financialData);
 }
 
-// set the initial values for financialData
+// set the initial values for financialData 
 setValues();
-setMix();
 
-// to refresh the values and chart on Calculate button press
+/**
+ * to refresh the values and chart on Calculate button press
+ */
 function refreshData() {
     setValues();
     chartGraphic.data.labels = financialData.ages;
